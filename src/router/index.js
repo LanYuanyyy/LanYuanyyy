9df7bda6eabdc10223ad3de66f386e3db96daabb
lanyuan <825293628@qq.com>
2020-03-31 15:18:09 +0800

    路由的懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../admin/login'
-import Home from '../admin/home/home'
-import User from '../admin/user/user'
-import Rights from '../admin/rights/rights'
-import Roles from '../admin/rights/roles'
-import Goods from '../admin/products/Goods'
-import Add from '../admin/products/Add'
-import Categories from '../admin/products/categories'
+// import Login from '../admin/login'
+// import Home from '../admin/home/home'
+// import User from '../admin/user/user'
+// import Rights from '../admin/rights/rights'
+// import Roles from '../admin/rights/roles'
+// import Goods from '../admin/products/Goods'
+// import Add from '../admin/products/Add'
+// import Categories from '../admin/products/categories'
 
-// const Login = () => (/* webpackChunkName: 'index' */ '../admin/login')
+const Login = () => import(/* webpackChunkName: 'index' */ '../admin/login')
+const Home = () => import(/* webpackChunkName: 'index' */ '../admin/home/home')
+const User = () => import(/* webpackChunkName: 'index' */ '../admin/user/user')
+const Rights = () => import(/* webpackChunkName: 'index' */ '../admin/rights/rights')
+const Roles = () => import(/* webpackChunkName: 'index' */ '../admin/rights/roles')
+const Goods = () => import(/* webpackChunkName: 'index' */ '../admin/products/Goods')
+const Add = () => (/* webpackChunkName: 'index' */ '../admin/products/Add')
+const Categories = () => import(/* webpackChunkName: 'index' */ '../admin/products/categories')
 
 
 Vue.use(VueRouter)
